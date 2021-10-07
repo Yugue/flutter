285d086509dd261128295df4b85e6e1fe70da0ff
Alex Chen <alexcn@google.com>
2021-10-07 17:12:17 -0400

    before adding click to show

--- dev/conductor/ui/lib/widgets/conductor_status.dart
+++ dev/conductor/ui/lib/widgets/conductor_status.dart
@@ -25,66 +25,62 @@ class ConductorStatusState extends State<ConductorStatus> {
   @override
   Widget build(BuildContext context) {
     final Map<String, Object> currentStatus = presentStateDesktop(widget.releaseState!);
-    final List<Map<String, Object>> engineCherrypicks =
-        currentStatus['engineCherrypicks']! as List<Map<String, Object>>;
+    final List<String> statusHeaderTitles = <String>[
+      'Conductor version:',
+      'Release channel:',
+      'Release version:',
+      'Release started at:',
+      'Release updated at:',
+      'Dart SDK evision:',
+    ];
+    final List<String> statusHeaderDataNames = <String>[
+      'conductorVersion',
+      'releaseChannel',
+      'releaseVersion',
+      'startedAt',
+      'updatedAt',
+      'dartRevision',
+    ];
 
     print(presentStateDesktop(widget.releaseState!));
     return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         if (widget.releaseState != null) ...<Widget>[
           Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Table(
+                columnWidths: const <int, TableColumnWidth>{
+                  0: FixedColumnWidth(200.0),
+                  1: FixedColumnWidth(400.0),
+                },
                 children: <TableRow>[
-                  TableRow(
-                    children: <Widget>[
-                      const Text('Conductor version'),
-                      SelectableText(currentStatus['conductorVersion']! as String),
-                    ],
-                  ),
-                  TableRow(
-                    children: <Widget>[
-                      const Text('Release channel'),
-                      SelectableText(currentStatus['releaseChannel']! as String),
-                    ],
-                  ),
-                  TableRow(
-                    children: <Widget>[
-                      const Text('Release version'),
-                      SelectableText(currentStatus['releaseVersion']! as String),
-                    ],
-                  ),
-                  TableRow(
-                    children: <Widget>[
-                      const Text('Release started at'),
-                      SelectableText(currentStatus['startedAt']! as String),
-                    ],
-                  ),
-                  TableRow(
-                    children: <Widget>[
-                      const Text('Release updated at'),
-                      SelectableText(currentStatus['updatedAt']! as String),
-                    ],
-                  ),
+                  for (int i = 0; i < statusHeaderTitles.length; i++)
+                    TableRow(
+                      children: <Widget>[
+                        Text(statusHeaderTitles[i]),
+                        SelectableText(currentStatus[statusHeaderDataNames[i]]! as String),
+                      ],
+                    ),
                 ],
               ),
-              DataTable(
-                columns: const <DataColumn>[
-                  DataColumn(label: Text('Engine Cherrypicks')),
-                  DataColumn(label: Text('Status')),
-                ],
-                rows: engineCherrypicks.map((Map<String, Object> engineCherrypick) {
-                  return DataRow(
-                    cells: <DataCell>[
-                      DataCell(
-                        Text(engineCherrypick['trunkRevision']! as String),
-                      ),
-                      DataCell(
-                        Text(engineCherrypick['state']! as String),
-                      ),
+              const SizedBox(height: 20.0),
+              Wrap(
+                runAlignment: WrapAlignment.start,
+                crossAxisAlignment: WrapCrossAlignment.start,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      const Text('123'),
+                      CherrypickTable(engineOrFramework: 'engine', currentStatus: currentStatus),
                     ],
-                  );
-                }).toList(),
+                  ),
+                  const SizedBox(width: 20.0),
+                  CherrypickTable(engineOrFramework: 'framework', currentStatus: currentStatus),
+                ],
               )
             ],
           )
@@ -94,6 +90,76 @@ class ConductorStatusState extends State<ConductorStatus> {
         const SizedBox(height: 40.0),
         SelectableText(presentState(widget.releaseState!)),
       ],
+    );
+  }
+}
+
+class StatusTooltip extends StatelessWidget {
+  const StatusTooltip({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const <Widget>[
+        Text('Status'),
+        SizedBox(width: 10.0),
+        Tooltip(
+          padding: EdgeInsets.all(10.0),
+          message: '''
+PENDING: The cherrypick has not yet been applied.
+PENDING_WITH_CONFLICT: The cherrypick has not been applied and will require manual resolution.
+COMPLETED: The cherrypick has been successfully applied to the local checkout.
+ABANDONED: The cherrypick will NOT be applied in this release.''',
+          child: Icon(Icons.info, size: 20.0),
+        ),
+      ],
+    );
+  }
+}
+
+class CherrypickTable extends StatefulWidget {
+  const CherrypickTable({
+    Key? key,
+    required this.engineOrFramework,
+    required this.currentStatus,
+  }) : super(key: key);
+
+  final String engineOrFramework;
+  final Map<String, Object> currentStatus;
+
+  @override
+  CherrypickTableState createState() => CherrypickTableState();
+}
+
+class CherrypickTableState extends State<CherrypickTable> {
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, Object>> cherrypicks = widget.engineOrFramework == 'engine'
+        ? widget.currentStatus['engineCherrypicks']! as List<Map<String, Object>>
+        : widget.currentStatus['frameworkCherrypicks']! as List<Map<String, Object>>;
+
+    return DataTable(
+      dataRowHeight: 30.0,
+      headingRowHeight: 30.0,
+      decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+      columns: <DataColumn>[
+        DataColumn(label: Text('${widget.engineOrFramework == 'engine' ? 'Engine' : 'Framework'} Cherrypicks')),
+        const DataColumn(
+          label: StatusTooltip(),
+        ),
+      ],
+      rows: cherrypicks.map((Map<String, Object> cherrypick) {
+        return DataRow(
+          cells: <DataCell>[
+            DataCell(
+              SelectableText(cherrypick['trunkRevision']! as String),
+            ),
+            DataCell(
+              SelectableText(cherrypick['state']! as String),
+            ),
+          ],
+        );
+      }).toList(),
     );
   }
 }
